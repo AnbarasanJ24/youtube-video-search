644c1a72a63583993527a7b8ca775420b8101b63
Anbarasan <anbarasanj24@gmail.com>
2021-09-02 10:26:37 +0530

    Added Grid Layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import './scss/App.scss';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
+    onSearchSubmit(searchTerm) {
+        console.log(searchTerm)
+    }
+
     render() {
-        return (<div>Hi</div>)
+        return (
+            <div className="container">
+                <div className="container__main">
+                    <div className="container__search">
+                        <SearchBar onSearchSubmit={this.onSearchSubmit} />
+                    </div>
+                    <div className="container__video">
+                        <div className="container__videoPlayer">
+                            Video Player
+                        </div>
+                        <div className="container__description">
+                            Video Description
+                        </div>
+                        <div className="container__videoList">
+                            Video List
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        )
     }
 }
 
---
+++ src/components/SearchBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './SearchBar.scss'
+
+class SearchBar extends React.Component {
+
+    state = { term: '' }
+
+    onInputChange = (event) => {
+        this.setState({ term: event.target.value });
+        this.props.onSearchSubmit(event.target.value);
+    }
+
+    render() {
+        return (
+            <div className="search-container">
+                <input
+                    value={this.state.term}
+                    type="search"
+                    placeholder="Search Videos"
+                    name="videoSearch"
+                    onChange={this.onInputChange}
+                    id="videoSearch" />
+            </div>
+        )
+    }
+
+}
+
+export default SearchBar;
